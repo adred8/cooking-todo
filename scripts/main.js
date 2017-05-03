274ef9ce401948d6d514768bcded1964763eca5f
Vaibhav <vkona8@gmail.com>
2017-05-03 19:46:05 +0530

    made significant changes...searching recipes, adding favorite recite to a list are functional

--- scripts/main.js
+++ scripts/main.js
@@ -4,86 +4,107 @@ function myFunction() {
 }
 
 (function(){
-  var app = angular.module("NarrowItDownApp", []);
+  var app = angular.module("NarrowItDownApp", ["ngRoute"]);
+  app.config(function($routeProvider){
+    $routeProvider
+      .when("/user", {
+        templateUrl : "templates/user.html",
+        controller : "userController",
+        controllerAs : "userCtrl"
+      })
+      .when("/recipe", {
+        templateUrl : "templates/recipe.html",
+        controller : "NarrowItDownController",
+        controllerAs : "Nid"
+      })
+  });
   app.controller("NarrowItDownController", NarrowItDownController);
+  app.controller("userController", userController);
   app.service("MenuSearchService", MenuSearchService);
 
+
+  userController.$inject = ["MenuSearchService"];
+  function userController (MenuSearchService){
+    var userCtrl = this;
+    userCtrl.favRecipeList = MenuSearchService.favRecipeList;
+    userCtrl.removeFavItem = function(index){
+      userCtrl.favRecipeList = MenuSearchService.removeFavItem(index);
+    }
+  };
 
   NarrowItDownController.$inject = ["MenuSearchService"];
   function NarrowItDownController (MenuSearchService, $q){
     var Nid = this;
     Nid.searchTerm = "";
     Nid.message = "";
-    Nid.foundMessage = "";
-    var getMyMenuItems = MenuSearchService.getMatchedMenuItems();
-    getMyMenuItems.then(function(response){
-      Nid.response = response;
-      Nid.found = Nid.response.Frames;
-      console.log("yea = ",  Nid.found);
-      console.log("Nid.response = ", Nid.response);
-      // console.log("sdsd = ",Nid.found.Frames );
-      // console.log("sdsd = ",Nid.found.Frames[0].images[0] );
-    });
-    getMyMenuItems.catch(function(error){
-      Nid.response = error;
-      console.log("Nid.response = ", Nid.response);
-    });
 
-
-    Nid.searchFrame = function(){
-      console.log("key pressed");
-      console.log("adas = ", Nid.searchTerm);
-      console.log("Nid.found = ", Nid.found);
-      console.log("Nid.response = ", Nid.response);
-      var newArrObj = [];
-      if (Nid.searchTerm == ""){
-        Nid.found = Nid.response.Frames;
-      }
-      else{
-        for (var i = 0; i < Nid.response.totalFrames; i++){
-          var str = Nid.response.Frames[i].FrameData.frameName.toLowerCase();
-          if (str.includes(Nid.searchTerm)){
-            newArrObj.push(Nid.response.Frames[i]);
-          }
-        }
-        Nid.found = newArrObj;
-      }
+    Nid.searchRecipes = function(){
+      Nid.message = "searching..."
+      var getMyMenuItems = MenuSearchService.getMatchedMenuItems(Nid.searchTerm);
+      getMyMenuItems.then(function(response){
+        Nid.response = response;
+        Nid.found = Nid.response.hits;
+        console.log("Nid.found = ",  Nid.found);
+        console.log("Nid.response = ", Nid.response);
+        Nid.message = "Done !";
+      });
+      getMyMenuItems.catch(function(error){
+        Nid.response = error;
+        console.log("Nid.response = ", Nid.response);
+        Nid.message = "Error found";
+      });
     };
+    Nid.favRecipeList = MenuSearchService.favRecipeList;
 
     Nid.addingItem = function(index){
-      Nid.myFrame = Nid.found[index];
-      console.log("frame = ", Nid.myFrame);
-    }
+      console.log("Nid.found[index]", Nid.found[index]);
+      Nid.favRecipeList = MenuSearchService.addingRecipe(index, Nid.found);
+      console.log("fav recipe list", Nid.favRecipeList);
+    };
+
+    console.log("final list", Nid.favRecipeList);
 
   };
-// Nid.found.Frames[0].FrameData.frameName
+
   MenuSearchService.$inject = ["$http", "$q", "$timeout"];
   function MenuSearchService($http, $q, $timeout){
     var service = this;
 
-    service.removeItem = function(idx, arr){
-      arr.splice(idx, 1);
-      return arr;
+    service.favRecipeList = [];
+    service.shopList = [];
+
+    service.addingRecipe = function(idx, arr){
+      service.favRecipeList.push(arr[idx]);
+      return service.favRecipeList;
     };
 
-    service.addItem = function(idx){
+    // service.removeItem = function(idx, arr){
+    //   arr.splice(idx, 1);
+    //   return arr;
+    // };
 
-    }
+    service.removeFavItem = function(idx){
+      service.favRecipeList.splice(idx,1);
+      console.log("favRecipeList = ", service.favRecipeList);
+      return service.favRecipeList;
+    };
 
-    service.getMatchedMenuItems = function (){
-      service.found = [];
-
+    service.getMatchedMenuItems = function (searchTerm){
       var message = "";
       var deferred = $q.defer();
       var getItems = $http({
         method: "GET",
-        // https://api.edamam.com/search
-        // url: ("https://yudql2tsmh.execute-api.us-east-1.amazonaws.com/testStichio/testing/b8680137-fde7-437d-80d9-d88b92633e97/?q=profileView"),
-        url: ("https://api.edamam.com/search"),
-        headers: {'Authorization': 'Token d0689f1e3ac6a820ce598c372b92e920a18a58cd'}
+        params: {
+          q : searchTerm,
+          app_id : "94c26889",
+          app_key : "e9ca84809360cb90d8bea638f7deab4c",
+          from : 0,
+          to : 10
+        },
+        url: ("https://api.edamam.com/search")
       });
       getItems.then(function(response){
-        // console.log("response.data.Frames = ", response.data);
+        console.log("response.data.Frames = ", response.data);
         deferred.resolve(response.data);
       });
 
